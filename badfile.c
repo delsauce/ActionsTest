72aa83fe33e87d16a1b50b292b2ff1ff880894a2
delsauce <delsauce@gmail.com>
2021-07-20 21:21:09 -0700

    Add files via upload

---
+++ badfile.c
@@ -0,0 +1,78 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2580
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fmodern\fcharset0 Courier;\f1\froman\fcharset0 Times-Roman;}
+{\colortbl;\red255\green255\blue255;\red0\green0\blue0;}
+{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;}
+\margl1440\margr1440\vieww18580\viewh12280\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs24 \cf2 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 // This is a bad file\
+\
+int
+\f1  
+\f0 main ( int
+\f1  
+\f0 argc, char* argv[])
+\f1 \
+
+\f0 \{
+\f1 \
+
+\f0 \'a0\'a0\'a0\'a0while
+\f1  
+\f0 (x == y) \
+	\{
+\f1 \
+
+\f0 \'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0something();
+\f1 \
+
+\f0 \'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0somethingelse();
+\f1 \
+
+\f0 \'a0\'a0\'a0
+\f1 \'a0\
+
+\f0 \'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0if
+\f1  
+\f0 (some_error) \{
+\f1 \
+
+\f0 \'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0do_correct();
+\f1 \
+
+\f0 \'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\}
+\f1  
+\f0 else
+\f1  
+\f0 if
+\f1  
+\f0 (another_error) \{
+\f1 \
+
+\f0 \'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0    \'a0\'a0\'a0do_correct();
+\f1 \
+
+\f0 \'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0\}\'a0\'a0 
+\f1 \
+
+\f0 \'a0\'a0\'a0\'a0\'a0\'a0\'a0\'a0else
+\f1  
+\f0 \{
+\f1  
+\f0 continue_as_usual();\}\
+\
+	if (true) \{ good(); \} else \{ bad(); \}
+\f1 \
+
+\f0 \'a0\'a0\'a0\'a0\}
+\f1 \
+\'a0\
+
+\f0 finalthing();
+\f1 \
+
+\f0 \}
+\f1 \
+}
