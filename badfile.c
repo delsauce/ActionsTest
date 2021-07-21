88492c1d8cd7a0989cb15927c8d6983560665cc8
delsauce <delsauce@users.noreply.github.com>
2021-07-21 05:22:29 +0000

    Committing clang-format changes

--- badfile.c
+++ badfile.c
@@ -1,21 +1,27 @@
 // This is a bad file
 
-int main ( int argc, char* argv[])
-{
-    while (x == y) 
-	{
-        something();
-        somethingelse();
-    
+int main(int argc, char *argv[]) {
+      while (x == y) {
+            something();
+            somethingelse();
+        
         if (some_error) {
-            do_correct();
-        } else if (another_error) {
-                do_correct();
-        }   
-        else { continue_as_usual();}
+                  do_correct();
+              
+    }
+    else if (another_error) {
+                      do_correct();
+              
+    }
+       
+        else { continue_as_usual(); }
 
-	if (true) { good(); } else { bad(); }
-    }
- 
-finalthing();
+    if (true) {
+      good();
+    } else {
+      bad();
+    }
+        
+  }
+    finalthing();
 }
